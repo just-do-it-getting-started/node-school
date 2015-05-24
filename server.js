2c504d1bc106937dc4327bd017fed48e5ac833da
KimJiTae <kimshinelove@gmail.com>
2015-05-24 12:54:13 +0900

    [refactor] export module that server

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var http = require('http');
 
-var onRequest = function(request, response) {
-	console.log("Request received.");
+var start = function() {
+	var onRequest = function(request, response) {
+		console.log("Request received.");
 
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	};
+
+	http.createServer(onRequest).listen(8888);
+
+	console.log("Server has started.");
 };
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started.");
+exports.start = start;
