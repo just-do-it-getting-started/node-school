b701d16d5160de4b18304462693fbca51354b0cf
KimJiTae <kimshinelove@gmail.com>
2015-05-25 12:04:40 +0900

    [feat] add post data

--- server.js
+++ server.js
@@ -5,14 +5,24 @@ var start = function(route, handle) {
 	var onRequest = function(request, response) {
 
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
+		console.log('Request for ' + pathname + ' received.');
 
-		var body = route(handle, pathname, response);
+		request.setEncoding('utf8');
+		
+		var postData = '';
+		request.addListener('data', function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+ postDataChunk + "'.");
+		});
+
+		request.addListener('end', function() {
+			route(handle, pathname, response, postData);
+		});
 	};
 
 	http.createServer(onRequest).listen(8888);
 
-	console.log("Server has started.");
+	console.log('Server has started.');
 };
 
 exports.start = start;
